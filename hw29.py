1e7f8ddd32e9e09272c38c32886e29d31e47e9c5
admin <klimovpcartem@gmail.com>
2023-07-15 15:58:01 +0300

    try 1

---
+++ hw29.py
@@ -0,0 +1,17 @@
+def generate_cube_numbers(end):
+    li = []
+    for x in range(2, end + 1):
+        j = x * x * x
+        if j <= end:
+            li.append(j)
+            yield j
+
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print('ok')
